1abe79a16f6baaefb43a20255f08c0c1424e19fb
Felix Hasse <felix.hasse@student.kit.edu>
2023-06-06 21:45:15 +0200

    Fix apollo config

--- src/app/util/apollo-client.ts
+++ src/app/util/apollo-client.ts
@@ -1,6 +1,8 @@
-import {ApolloClient, InMemoryCache} from "@apollo/client";
+import {ApolloClient, createHttpLink, InMemoryCache} from "@apollo/client";
 import {ApolloLink} from "apollo-link";
 import {HttpLink} from "apollo-link-http";
+import {onError} from "@apollo/client/link/error";
+
 const SPACE_ID = process.env.NEXT_PUBLIC_SPACE_ID;
 const ENVIRONMENT = process.env.NEXT_PUBLIC_ENVIRONMENT;
 const ACCESS_TOKEN = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
@@ -8,20 +10,31 @@ const ACCESS_TOKEN = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
 
 const URL = `https://graphql.contentful.com/content/v1/spaces/${SPACE_ID}/environments/${ENVIRONMENT}`;
 
-const http = new HttpLink({
+const http = createHttpLink({
     uri: URL,
     headers: {
         Authorization: `Bearer ${ACCESS_TOKEN}`,
     },
 });
 
-const link = ApolloLink.from([http]);
+const errorLink = onError(({graphQLErrors, networkError}) => {
+    if (graphQLErrors)
+        graphQLErrors.map(({message, locations, path}) =>
+            console.log(
+                `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+            ),
+        );
+
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+
 
 const cache = new InMemoryCache();
 
 // @ts-ignore
 const apolloClient = new ApolloClient({
-    link,
+    link: errorLink.concat(http),
     cache,
 });
 export default apolloClient;
